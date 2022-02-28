7b25a247894c121904d7d101f99db0640a143bc9
Xie Zhihao <zhihao.xie@intel.com>
2022-02-28 17:56:28 +0800

    contrib: fix DML benchmark
    Signed-off-by: Xie Zhihao <zhihao.xie@intel.com>

--- contrib/dml/test/dml_speed_test.cc
+++ contrib/dml/test/dml_speed_test.cc
@@ -1,25 +1,58 @@
-#include "source/common/buffer/buffer_impl.h"
+#include <dml/hl/status_code.hpp>
+
 #include "source/common/common/assert.h"
 
 #include "absl/strings/string_view.h"
 #include "benchmark/benchmark.h"
+#include "dml/dml.hpp"
 
 namespace Envoy {
 
 // NOLINTNEXTLINE(readability-identifier-naming)
 static void BM_memcpy(benchmark::State& state) {
   const std::string data(state.range(0), 'a');
-  const absl::string_view input(data);
-  Buffer::OwnedImpl buffer1(input);
-  Buffer::OwnedImpl buffer2(input);
+  std::string buffer;
+  buffer.reserve(data.size());
   for (auto _ : state) {
     UNREFERENCED_PARAMETER(_);
-    buffer1.move(buffer2); // now buffer1 has 2 copies of the input, and buffer2 is empty.
-    buffer2.move(buffer1, input.size()); // now buffer1 and buffer2 are the same size.
+    memcpy(buffer.data(), data.data(), data.size());
   }
-  uint64_t length = buffer1.length();
-  benchmark::DoNotOptimize(length);
+  benchmark::DoNotOptimize(buffer.size());
 };
 BENCHMARK(BM_memcpy)->Arg(1)->Arg(4096)->Arg(16384)->Arg(65536);
+
+// NOLINTNEXTLINE(readability-identifier-naming)
+static void BM_DmlSoftware(benchmark::State& state) {
+  const std::string data(state.range(0), 'a');
+  std::string buffer;
+  buffer.resize(data.size());
+  for (auto _ : state) {
+    UNREFERENCED_PARAMETER(_);
+    auto result =
+        dml::execute<dml::software>(dml::mem_copy, dml::make_view(data.data(), data.size()),
+                                    dml::make_view(buffer.data(), buffer.size()));
+    ASSERT(result.status == dml::status_code::ok, "");
+    ASSERT(data == buffer, "");
+  }
+  benchmark::DoNotOptimize(buffer.size());
+};
+BENCHMARK(BM_DmlSoftware)->Arg(1)->Arg(4096)->Arg(16384)->Arg(65536);
+
+// NOLINTNEXTLINE(readability-identifier-naming)
+static void BM_DmlHardware(benchmark::State& state) {
+  const std::string data(state.range(0), 'a');
+  std::string buffer;
+  buffer.resize(data.size());
+  for (auto _ : state) {
+    UNREFERENCED_PARAMETER(_);
+    auto result =
+        dml::execute<dml::hardware>(dml::mem_copy, dml::make_view(data.data(), data.size()),
+                                    dml::make_view(buffer.data(), buffer.size()));
+    ASSERT(result.status == dml::status_code::ok, "");
+    ASSERT(data == buffer, "");
+  }
+  benchmark::DoNotOptimize(buffer.size());
+};
+BENCHMARK(BM_DmlHardware)->Arg(1)->Arg(4096)->Arg(16384)->Arg(65536);
 
 } // namespace Envoy
