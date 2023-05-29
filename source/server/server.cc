26a9c882a3b1e89bcbfaebf1f21523b3ce4d540e
Xie Zhihao <zhihao.xie@intel.com>
2023-05-29 09:10:25 +0000

    server: revert socket interface initialization change
    Signed-off-by: Xie Zhihao <zhihao.xie@intel.com>

--- source/server/server.cc
+++ source/server/server.cc
@@ -620,18 +620,13 @@ void InstanceImpl::initialize(Network::Address::InstanceConstSharedPtr local_add
         std::move(safe_actions), std::move(unsafe_actions), api_->threadFactory());
   }
 
-  Network::SocketInterface* sock = nullptr;
   if (!bootstrap_.default_socket_interface().empty()) {
     auto& sock_name = bootstrap_.default_socket_interface();
-    sock = const_cast<Network::SocketInterface*>(Network::socketInterface(sock_name));
-    ASSERT(sock != nullptr);
-    Network::SocketInterfaceSingleton::clear();
-    Network::SocketInterfaceSingleton::initialize(sock);
-  } else {
-    auto factory = dynamic_cast<Server::Configuration::BootstrapExtensionFactory*>(
-        Network::SocketInterfaceSingleton::getExisting());
-    bootstrap_extensions_.push_back(factory->createBootstrapExtension(
-        *factory->createEmptyConfigProto(), serverFactoryContext()));
+    auto sock = const_cast<Network::SocketInterface*>(Network::socketInterface(sock_name));
+    if (sock != nullptr) {
+      Network::SocketInterfaceSingleton::clear();
+      Network::SocketInterfaceSingleton::initialize(sock);
+    }
   }
 
   ListenerManagerFactory* listener_manager_factory = nullptr;
